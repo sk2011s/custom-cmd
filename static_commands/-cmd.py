843beba77e4a1efaaefff93b4a05c475cbb87170
sk2011se <133661602+sk2011s@users.noreply.github.com>
2024-10-10 08:48:32 +0330

    Update -cmd.py

--- static_commands/-cmd.py
+++ static_commands/-cmd.py
@@ -11,7 +11,7 @@ def run(*args):
     try:
         # Joining arguments into a single command string
         command = " ".join(args)
-        print(f"Executing command: {command}")
+#        print(f"Executing command: {command}")
         bat(command)
     except Exception as e:
         print(f"Error: {e}")
