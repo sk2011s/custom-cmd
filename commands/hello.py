9ece41d277d1f436e3b73d08d6f2d5fc15ab164e
sk2011s <eatebarisajjad@gmail.com>
2024-10-20 18:23:33 +0330

    gui 3

--- commands/hello.py
+++
@@ -1,8 +0,0 @@
-no_args = False
-args = ["name"]
-ids = ['h']
-arg_type = "arg"
-description = ""
-
-def run(*args):
-    print(f"Hello {" ".join(args)}")
--- commands/w.py
+++
@@ -1,8 +0,0 @@
-no_args = False
-args = ['a', 'g', 'r', 'sp']
-ids = ['s', 'g', 'h']
-arg_type = "args"
-description = "asdadfaf"
-
-def run(a,g,r,sp):
-    pass
